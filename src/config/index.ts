afb18a87d039e055a38df1f19b34bbdf7f5ba150
Azizkhuja <azizxoja200425@mail.ru>
2025-06-17 19:56:26 +0500

    added ci/cd deploy file

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 import path from 'path';
 
 const env = process.env.NODE_ENV || 'development';
-const envFile = `.env.${env}`;
+//const envFile = `.env.${env}`;
 
 config({
   path: path.resolve(process.cwd(), envFile)
